8e045fcb1b8bd50f4762239d2b0d8393f12254c4
D <d@localhost.localdomain>
2020-03-14 08:45:31 +0300

    remove all sh*t

--- main.cpp
+++ main.cpp
@@ -1,34 +1,18 @@
-#include <limits>
-#include <cmath>
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include "geometry.h"
 
-void render() {
-    const int width    = 1024;
-    const int height   = 768;
-    std::vector<Vec3f> framebuffer(width*height);
-
-    for (size_t j = 0; j<height; j++) {
-        for (size_t i = 0; i<width; i++) {
-            framebuffer[i+j*width] = Vec3f(j/float(height),i/float(width), 0);
+int main() {
+    int nx = 200;
+    int ny = 100;
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    for (int j = ny-1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float r = float(i) / float(nx);
+            float g = float(j) / float(ny);
+            float b = 0.2;
+            int ir = int(255.99*r);
+            int ig = int(255.99*g);
+            int ib = int(255.99*b);
+            std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
-
-    std::ofstream ofs; // save the framebuffer to file
-    ofs.open("./out.ppm");
-    ofs << "P6\n" << width << " " << height << "\n255\n";
-    for (size_t i = 0; i < height*width; ++i) {
-        for (size_t j = 0; j<3; j++) {
-            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
-        }
-    }
-    ofs.close();
 }
-
-int main(){
-    render();
-    return 0;
-}
-
